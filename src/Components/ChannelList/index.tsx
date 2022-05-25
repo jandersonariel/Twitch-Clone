39bed707d51755ebe561e8cbd628698ffca7bf7d
janderson <janderson.ariel@yahoo.com.br>
2022-05-25 13:17:33 -0300

    first commit

---
+++ src/Components/ChannelList/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+import {
+  List,
+  ChannelContainer,
+  LeftSide,
+  Avatar,
+  Column,
+  Username,
+  Info,
+  RightSide,
+  WhiteCircle,
+} from './styles';
+
+const ChannelList: React.FC = () => {
+  const ChannelItem = () => (
+    <ChannelContainer>
+      <LeftSide>
+        <Avatar />
+        <Column>
+          <Username>Dev_three</Username>
+          <Info>36 news videos</Info>
+        </Column>
+      </LeftSide>
+
+      <RightSide>
+        <WhiteCircle />
+      </RightSide>
+    </ChannelContainer>
+  );
+
+  return (
+    <List>
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+    </List>
+  );
+};
+
+export default ChannelList;
---
+++ src/Components/Title/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components/native"
+import colors from "../../styles/colors";
+
+export const Container = styled.Text`
+color: ${colors.gray};
+background: ${colors.primary};
+
+font-family: roboto_700;
+font-size: 14px;
+text-transform: uppercase;
+
+padding: 9px 0;
+`;
+
+export const Title = styled.View`
+
+`;
